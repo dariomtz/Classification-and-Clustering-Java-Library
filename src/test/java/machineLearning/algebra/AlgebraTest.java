e3544ce2883c109481eb82a0b7dc574f80150211
natxsglez <44679189+natxsglez@users.noreply.github.com>
2020-05-05 18:53:02 -0500

    Update AlgebraTest.java

--- src/test/java/machineLearning/algebra/AlgebraTest.java
+++ src/test/java/machineLearning/algebra/AlgebraTest.java
@@ -12,6 +12,20 @@ public class AlgebraTest {
         v.add(1d);
         v.add(1d);
 
-        Assert.assertEquals("Norm should return the sqrt of 2", Algebra.norm(v), Math.sqrt(2));
+        Assert.assertEquals("Norm should return the sqrt of 2", Algebra.norm(v), Math.sqrt(2),0);
+    }
+    public void testSumVectorMethod(){
+        Vector<Integer> v1 = new Vector<>();
+        v1.add(5);
+        v1.add(3);
+        Vector<Double> v2 = new Vector<>();
+        v2.add(3.55);
+        v2.add(8.63);
+
+        Vector<Double> v3 = new Vector<>();
+        v3.add(8.55);
+        v3.add(11.63);
+
+        Assert.assertEquals("Sum should return the sum of 2 vectors in a new vector",v3,Algebra.sumVector(v1,v2));
     }
 }
