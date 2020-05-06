2410b7d4f6caa10e38eb636b8018735de80b5353
MAGP116 <44816914+MAGP116@users.noreply.github.com>
2020-05-05 20:13:42 -0500

    Now it actually works

--- src/main/java/machineLearning/algebra/Algebra.java
+++ src/main/java/machineLearning/algebra/Algebra.java
@@ -11,30 +11,29 @@ public class Algebra {
         }
         return Math.sqrt(norm);
     }
-
-    public static <T extends Number> Vector<Double> sumVector(Vector<T> a, Vector<T> b){
+    public static <U extends Number,V extends Number> Vector<Double> sumVector(Vector<U> a, Vector<V> b){
         if(a.size()!= b.size()) return null;
         Vector<Double> sum = new Vector<>();
 
         int i;
         for(i=0;i<a.size();i++){
-            sum.add((double)(a.get(i))+(double)(b.get(i)));
+            sum.add(a.get(i).doubleValue()+b.get(i).doubleValue());
         }
         return sum;
     }
 
-    public static <T extends Number> Vector<Double> substractVector(Vector<T> a, Vector<T> b){
+    public static <U extends Number,V extends Number> Vector<Double> substractVector(Vector<U> a, Vector<V> b){
         if(a.size()!= b.size()) return null;
         Vector<Double> sub = new Vector<>();
 
         int i;
         for(i=0;i<a.size();i++){
-            sub.add((double)a.get(i)+(double)b.get(i));
+            sub.add(a.get(i).doubleValue()+b.get(i).doubleValue());
         }
         return sub;
     }
 
-    public static <T extends Number> Vector<Double> cross(Vector<T> a, Vector<T> b){
+    public static <U extends Number,V extends Number> Vector<Double> cross(Vector<U> a, Vector<V> b){
         //Suponiendo que los vectores tienen 3 componentes
         if(a.size()!= b.size()) return null;
         Vector<Double> cross = new Vector<>();
@@ -42,23 +41,23 @@ public class Algebra {
         if(a.size() == 2){
             cross.add(0,0.0);
             cross.add(1,0.0);
-            cross.add(2,((double)a.get(0)*(double)b.get(1))-((double)a.get(1)*(double)b.get(0)));
+            cross.add(2,(a.get(0).doubleValue()*b.get(1).doubleValue())-(a.get(1).doubleValue()*b.get(0).doubleValue()));
         }
         else if(a.size()==3){
-            cross.add(0,((double)a.get(1)*(double)b.get(2))-((double)a.get(2)*(double)b.get(1)));
-            cross.add(1,((double)a.get(0)*(double)b.get(2))-((double)a.get(2)*(double)b.get(0)));
-            cross.add(2,((double)a.get(0)*(double)b.get(1))-((double)a.get(1)*(double)b.get(0)));
+            cross.add(0,(a.get(1).doubleValue()*b.get(2).doubleValue())-(a.get(2).doubleValue()*b.get(1).doubleValue()));
+            cross.add(1,(a.get(0).doubleValue()*b.get(2).doubleValue())-(a.get(2).doubleValue()*b.get(0).doubleValue()));
+            cross.add(2,(a.get(0).doubleValue()*b.get(1).doubleValue())-(a.get(1).doubleValue()*b.get(0).doubleValue()));
         }
         return cross;
     }
 
-    public static <T extends Number> double dot(Vector<T> a, Vector<T> b){
+    public static <U extends Number,V extends Number> double dot(Vector<U> a, Vector<V> b){
         if(a.size()!= b.size()) return 0;
         double dot = 0;
 
         int i;
         for(i=0;i<a.size();i++){
-            dot += (double)a.get(i)*(double)b.get(i);
+            dot += a.get(i).doubleValue()*b.get(i).doubleValue();
         }
         return dot;
     }
@@ -66,7 +65,7 @@ public class Algebra {
     public static <T extends Number> void multEsc(T n, Vector<T> v){
         int i;
         for(i=0;i<v.size();i++){
-            v.add(i,(T)((Number)((double)v.get(i)*(double)n)));
+            v.add(i,(T)((Number)(v.get(i).doubleValue()*n.doubleValue())));
         }
     }
 
