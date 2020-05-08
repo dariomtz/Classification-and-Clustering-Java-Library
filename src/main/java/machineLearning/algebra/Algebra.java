7d70e2b1f8c0a3142b27fdd220454e6bf0fc0743
Dario Martinez <gusdamtzr@gmail.com>
2020-05-08 18:12:50 -0500

    renamed all methods which names were confusing
    Now every method that does a simple aritmetical operation is called either sum, substract, mutl or division.

--- src/main/java/machineLearning/algebra/Algebra.java
+++ src/main/java/machineLearning/algebra/Algebra.java
@@ -3,13 +3,23 @@ package machineLearning.algebra;
 import java.util.Vector;
 
 public class Algebra {
+
     public static <T extends Number> double norm(Vector<T> v){
         double norm = 0;
         int i;
         for(i=0;i<v.size();i++) norm += Math.pow(v.get(i).doubleValue(),2);
         return Math.sqrt(norm);
     }
-    public static <U extends Number,V extends Number> Vector<Double> sumVector(Vector<U> a, Vector<V> b){
+
+    public static <U extends Number> double sum(Vector<U> v){
+        double sum = 0.0;
+        int i;
+        for(i=0;i<v.size();i++)
+            sum += v.get(i).doubleValue();
+        return sum;
+    }
+
+    public static <U extends Number,V extends Number> Vector<Double> sum(Vector<U> a, Vector<V> b){
         if(a.size()!= b.size()) return null;
         Vector<Double> sum = new Vector<>();
         int i;
@@ -17,12 +27,76 @@ public class Algebra {
         return sum;
     }
 
-    public static <U extends Number,V extends Number> Vector<Double> substractVector(Vector<U> a, Vector<V> b){
+    public static <U extends Number> Vector<Double> sum(Vector <U> v, double x){
+        Vector<Double> sum = new Vector<>();
+        for (Number n :
+                v) {
+            sum.add(n.doubleValue() + x);
+        }
+        return sum;
+    }
+
+    public static <U extends Number,V extends Number> Vector<Double> subtract(Vector<U> a, Vector<V> b){
         if(a.size()!= b.size()) return null;
         Vector<Double> sub = new Vector<>();
         int i;
         for(i=0;i<a.size();i++) sub.add(a.get(i).doubleValue()-b.get(i).doubleValue());
         return sub;
+    }
+
+    public static <U extends Number> Vector<Double> subtract(Vector<U> v, double x){
+        Vector<Double> sub = new Vector<>();
+        int i;
+        for(Number n : v)
+            sub.add(n.doubleValue()-x);
+        return sub;
+    }
+
+    public static <U extends Number> Vector<Double> subtract(double x, Vector<U> v){
+        Vector<Double> sub = new Vector<>();
+        int i;
+        for(Number n : v)
+            sub.add(x - n.doubleValue());
+        return sub;
+    }
+
+    public static <U extends Number,V extends Number> Vector<Double> mult(U n, Vector<V> v){
+        int i;
+        Vector<Double> mult = new Vector<>();
+        for(i=0;i<v.size();i++) mult.add(v.get(i).doubleValue()*n.doubleValue());
+        return mult;
+    }
+
+    public static <U extends Number, V extends Number> Vector<Double> mult(Vector<U> a, Vector <V> b){
+        if(a.size()!= b.size()) return null;
+        Vector<Double> mult = new Vector<>();
+        for (int i = 0; i < a.size(); i++) {
+            mult.add(a.get(i).doubleValue() * b.get(i).doubleValue());
+        }
+        return mult;
+    }
+
+    public static <U extends Number, V extends Number> Vector<Double> division(Vector<U> a, Vector<V> b){
+        if(a.size()!= b.size()) return null;
+        Vector<Double> div = new Vector<>();
+        for (int i = 0; i < a.size(); i++) {
+            div.add(a.get(i).doubleValue() / b.get(i).doubleValue());
+        }
+        return div;
+    }
+
+    public static <U extends Number,V extends Number> Vector<Double> division(U n, Vector<V> v){
+        Vector<Double> div = new Vector<>();
+        for(int i=0;i<v.size();i++)
+            div.add(n.doubleValue() / v.get(i).doubleValue());
+        return div;
+    }
+
+    public static <U extends Number,V extends Number> Vector<Double> division(Vector<V> v, U n){
+        Vector<Double> div = new Vector<>();
+        for(int i=0;i<v.size();i++)
+            div.add(v.get(i).doubleValue() / n.doubleValue() ) ;
+        return div;
     }
 
     public static <U extends Number,V extends Number> Vector<Double> cross(Vector<U> a, Vector<V> b){
@@ -52,22 +126,6 @@ public class Algebra {
             dot += a.get(i).doubleValue()*b.get(i).doubleValue();
         }
         return dot;
-    }
-
-    public static <U extends Number,V extends Number> Vector<Double> multEsc(U n, Vector<V> v){
-        int i;
-        Vector<Double> mult = new Vector<>();
-        for(i=0;i<v.size();i++) mult.add(v.get(i).doubleValue()*n.doubleValue());
-        return mult;
-    }
-
-
-    public static <U extends Number> double sum(Vector<U> v){
-        double sum = 0.0;
-        int i;
-        for(i=0;i<v.size();i++)
-            sum += v.get(i).doubleValue();
-        return sum;
     }
 
     public static <U extends Number, V extends Number> double euclideanDistance(Vector<U> u, Vector<V> v){
