a854e2b3f0080d0886db4d56812eb23c1c55850f
natxsglez <44679189+natxsglez@users.noreply.github.com>
2020-05-08 16:10:28 -0500

    Updated DifferetVectorSize exception

--- src/main/java/machineLearning/Exceptions/DifferentVectorSizeFound.java
+++ src/main/java/machineLearning/Exceptions/DifferentVectorSizeFound.java
@@ -1,4 +1,16 @@
 package machineLearning.Exceptions;
 
-public class DifferentVectorSizeFound {
+public class DifferentVectorSizeFound extends Exception{
+    protected int sizeA;
+    protected int sizeB;
+
+    public DifferentVectorSizeFound(int a, int b){
+        super("DifferentVectorSizeFound");
+        sizeA = a;
+        sizeB = b;
+    }
+
+    public String toString(){
+        return getMessage() + "\nDifferent sizes found\n A: " + sizeA + "\nB: " + sizeB;
+    }
 }
