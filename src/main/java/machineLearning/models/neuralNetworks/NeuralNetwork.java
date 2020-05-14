df6398d7e58c7c0a3a037081a72c218a071f2287
Dario Martinez <gusdamtzr@gmail.com>
2020-05-14 00:34:34 -0500

    Last corrections to neural network

--- src/main/java/machineLearning/models/neuralNetworks/NeuralNetwork.java
+++ src/main/java/machineLearning/models/neuralNetworks/NeuralNetwork.java
@@ -9,6 +9,7 @@ import java.util.Vector;
 public abstract class NeuralNetwork extends SupervisedModel {
     protected ActivationFunction af = ActivationFunction.LEAKY_RELU;
     protected double gamma = 1;
+    protected boolean logError = false;
 
     public NeuralNetwork(Vector<Vector<Double>> inputs, Vector<Vector<Double>> outputs) {
         super(inputs, outputs);
@@ -74,6 +75,27 @@ public abstract class NeuralNetwork extends SupervisedModel {
         setGamma(gamma);
     }
 
+    public NeuralNetwork(Vector<Vector<Double>> inputs, Vector<Vector<Double>> outputs, ActivationFunction af,
+                         double gamma, boolean logError) {
+        super(inputs, outputs);
+        if (inputs.size() != outputs.size())
+            throw new DifferentMatrixSizeFoundException(inputs.size(), inputs.get(0).size(),
+                    outputs.size(), outputs.get(0).size());
+        setAf(af);
+        setGamma(gamma);
+        setLogError(logError);
+    }
+
+    public NeuralNetwork(Matrix inputs, Matrix outputs, ActivationFunction af, double gamma, boolean logError) {
+        super(inputs, outputs);
+        if(inputs.rows != outputs.rows)
+            throw new DifferentMatrixSizeFoundException(inputs.rows, inputs.cols,
+                    outputs.rows, outputs.cols);
+        setAf(af);
+        setGamma(gamma);
+        setLogError(logError);
+    }
+
     public void setAf(ActivationFunction af) {
         this.af = af;
     }
@@ -81,6 +103,13 @@ public abstract class NeuralNetwork extends SupervisedModel {
     public void setGamma(double gamma) {
         this.gamma = gamma;
     }
+
+    public void setLogError(boolean logError){
+        this.logError = logError;
+    }
+
+    protected abstract void populateWeights();
+    protected abstract void populateBias();
 
     @Override
     public Matrix classify(Matrix input) {
